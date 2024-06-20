655023c83c2ca32c0ccc9a8dff5ac4afd773c58b
Senthurbavan <senthurbavankiruba@gmail.com>
2024-06-20 15:06:12 -0400

    gym components in franka_play are removed

--- examples/franka_play.py
+++ examples/franka_play.py
@@ -24,8 +24,6 @@
 
 """
 import copy
-from isaacgym import gymapi
-from isaacgym import gymutil
 
 import torch
 torch.multiprocessing.set_start_method('spawn',force=True)
@@ -33,47 +31,16 @@ torch.set_num_threads(8)
 torch.backends.cudnn.benchmark = False
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
-#
 
+# import matplotlib
+# matplotlib.use('tkagg')
 
-
-import matplotlib
-matplotlib.use('tkagg')
-
-import matplotlib.pyplot as plt
-
-import time
-import yaml
 import argparse
 import numpy as np
-from quaternion import quaternion, from_rotation_vector, from_rotation_matrix
-import matplotlib.pyplot as plt
-
-from quaternion import from_euler_angles, as_float_array, as_rotation_matrix, from_float_array, as_quat_array
-
-from storm_kit.gym.core import Gym, World
-from storm_kit.gym.sim_robot import RobotSim
-from storm_kit.util_file import get_configs_path, get_gym_configs_path, join_path, load_yaml, get_assets_path
-from storm_kit.gym.helpers import load_struct_from_dict
-
-from storm_kit.util_file import get_mpc_configs_path as mpc_configs_path
-
-from storm_kit.differentiable_robot_model.coordinate_transform import quaternion_to_matrix, CoordinateTransform
 from storm_kit.mpc.task.reacher_task import ReacherTask
 np.set_printoptions(precision=2)
 
-# To record
-# Global
-# x_des
-# sim_dt - to check
-# w_T_r
-# recorded_length
 
-# Sequence
-# goal_pose = (g_pose, g_q)
-# t_step
-# current_robot_state
-# command
 
 
 def mpc_robot_interactive(args):
@@ -94,10 +61,6 @@ def mpc_robot_interactive(args):
     sim_dt = mpc_control.exp_params['control_dt']
     assert sim_dt == recorded_data['sim_dt']
 
-    w_T_r_data = recorded_data['w_T_r']
-    w_T_r_p = gymapi.Vec3(w_T_r_data[0], w_T_r_data[1], w_T_r_data[2])
-    w_T_r_r = gymapi.Quat(w_T_r_data[3], w_T_r_data[4], w_T_r_data[5], w_T_r_data[6])
-    w_T_r = gymapi.Transform(p=w_T_r_p, r=w_T_r_r) # load from recorded data
     recorded_length = recorded_data['recorded_length'] # the length of the recorded data
 
     goal_pose_lst = recorded_data['goal_pose']
@@ -131,12 +94,12 @@ def mpc_robot_interactive(args):
         q_des_target = copy.deepcopy((command_lst[i])['position'])
 
         # print(f'i:{i} recorded and computed q_des\n\t{q_des_target}\n\t{q_des}')
-        # cmd_error = (q_des_target - q_des)
-        # cmd_error_ratio = abs(cmd_error / q_des_target)*100
-        # if sum(cmd_error_ratio>5):
-        #     print(f'step: {i} error:', end='\t[')
-        #     [print(f' {e:.2f} ', end='') for e in cmd_error_ratio]
-        #     print(']')
+        cmd_error = (q_des_target - q_des)
+        cmd_error_ratio = abs(cmd_error / q_des_target)*100
+        if sum(cmd_error_ratio>5):
+            print(f'step: {i} error:', end='\t[')
+            [print(f' {e:.2f} ', end='') for e in cmd_error_ratio]
+            print(']')
     print(f'Recorded length: {recorded_length}')
     print('End')
 
