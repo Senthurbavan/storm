c5c0788b88055b6478a38f2737e87236979076d2
Senthurbavan <senthurbavankiruba@gmail.com>
2024-07-02 22:26:44 -0400

    python file to calculate loss for multiple parameter configs added

---
+++ examples/calc_traj_error.py
@@ -0,0 +1,153 @@
+#
+# MIT License
+#
+# Copyright (c) 2020-2021 NVIDIA CORPORATION.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.#
+""" Example spawning a robot in gym
+
+"""
+import copy
+from isaacgym import gymapi
+from isaacgym import gymutil
+
+import torch
+torch.multiprocessing.set_start_method('spawn',force=True)
+torch.set_num_threads(8)
+torch.backends.cudnn.benchmark = False
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+#
+
+
+
+import matplotlib
+matplotlib.use('tkagg')
+
+import matplotlib.pyplot as plt
+
+import time
+import yaml
+import argparse
+import numpy as np
+from quaternion import quaternion, from_rotation_vector, from_rotation_matrix
+import matplotlib.pyplot as plt
+
+from quaternion import from_euler_angles, as_float_array, as_rotation_matrix, from_float_array, as_quat_array
+
+from storm_kit.gym.core import Gym, World
+from storm_kit.gym.sim_robot import RobotSim
+from storm_kit.util_file import get_configs_path, get_gym_configs_path, join_path, load_yaml, get_assets_path
+from storm_kit.gym.helpers import load_struct_from_dict
+
+from storm_kit.util_file import get_mpc_configs_path as mpc_configs_path
+
+from storm_kit.differentiable_robot_model.coordinate_transform import quaternion_to_matrix, CoordinateTransform
+from storm_kit.mpc.task.reacher_task import ReacherTask
+np.set_printoptions(precision=2)
+
+PARAM_LABEL = ['primitive_collision', 'robot_self_collision', 'manipulability', 'stop_cost']
+
+def create_param_dic(param):
+    param_dict = dict(zip(PARAM_LABEL, param))
+    return param_dict
+
+def change_params(mpc_c, param_dict):
+    if 'horizon' in param_dict.keys():
+        mpc_c.change_horizon(param_dict['horizon'])
+        param_dict.pop('horizon')
+
+    if 'num_particles' in param_dict.keys():
+        mpc_c.change_num_particles(param_dict['num_particles'])
+        param_dict.pop('num_particles')
+
+    # change cost weights
+    cost_params = {}
+    for k, v in param_dict.items():
+        cost_params[k] = {'weight':v}
+    mpc_c.controller.rollout_fn.change_cost_params(cost_params)
+
+def mpc_evaluate(param_dict, idx=0):
+
+    robot = 'franka'
+    robot_file = robot + '.yml'
+    task_file = robot + '_reacher.yml'
+    world_file = 'collision_primitives_3d.yml'
+
+    device = torch.device('cuda', 0)
+    tensor_args = {'device': device, 'dtype': torch.float32}
+
+    mpc_control = ReacherTask(task_file, robot_file, world_file, tensor_args, idx=idx)
+    change_params(mpc_control, param_dict)
+
+    x_pos, x_q = recorded_data['x_des']
+    mpc_control.update_params(goal_ee_pos=x_pos, goal_ee_quat=x_q)
+
+    sim_dt = mpc_control.exp_params['control_dt']
+    assert sim_dt == recorded_data['sim_dt']
+
+    recorded_length = recorded_data['recorded_length']  # the length of the recorded data
+    goal_pose_lst = recorded_data['goal_pose']
+    t_step_lst = recorded_data['t_step']
+    current_robot_state_lst = recorded_data['current_robot_state']
+    command_lst = recorded_data['command']
+
+    assert recorded_length == len(goal_pose_lst) == len(t_step_lst) == len(current_robot_state_lst) == len(command_lst)
+
+    dof = command_lst[0]['position'].shape[0]
+    cmd_error = np.zeros(dof)
+
+    for i in range(recorded_length):
+        goal_pose = goal_pose_lst[i]
+        if goal_pose is not None:
+            g_pos, g_q = goal_pose
+            mpc_control.update_params(goal_ee_pos=g_pos,
+                                      goal_ee_quat=g_q)
+
+        t_step = t_step_lst[i]
+        current_robot_state = current_robot_state_lst[i]
+
+        command = mpc_control.get_command(t_step, current_robot_state, control_dt=sim_dt, WAIT=True)
+        q_des = copy.deepcopy(command['position'])
+        q_des_target = copy.deepcopy((command_lst[i])['position'])
+        cmd_error += (q_des_target - q_des)**2
+
+    mpc_control.close()
+
+    cmd_error /= recorded_length
+    cmd_error = np.sum(cmd_error)/dof
+    print(f'cmd_error {cmd_error:.7f}')
+    print('==END==\n')
+    return cmd_error
+
+if __name__ == '__main__':
+    recorded_data = torch.load('record_data')
+    print(f"\nRecorded data length: {recorded_data['recorded_length']}\n")
+    params = [[6000, 5000, 40, 100],
+              [4000, 7000, 30, 110],
+              [5000, 5000, 30, 100],
+              [3000, 5000, 50, 200],
+              [5500, 3000, 40, 100]]
+    loss = [None]*len(params)
+
+    for i in range(len(params)):
+        loss[i] = mpc_evaluate(create_param_dic(params[i]))
+
+    for i in range(len(params)):
+        print(f'Param:{params[i]} loss: {loss[i]:.7f}')
